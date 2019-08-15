904cb95629489aa7e87e5a3a3e6dcebb5e9fae69
zhufengnodejs <zhufengnodejs@126.com>
2019-08-15 23:39:11 +0800

    msg

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http');
 let users = [
-    {id:111,name:'zhufeng1'},
-    {id:222,name:'zhufeng2'},
-    {id:333,name:'zhufeng3'}
+    {id:44,name:'zhufeng1'},
+    {id:55,name:'zhufeng2'},
+    {id:66,name:'zhufeng3'}
 ]
 let server = http.createServer(function(req,res){
   res.setHeader('Access-Control-Allow-Origin','*');
